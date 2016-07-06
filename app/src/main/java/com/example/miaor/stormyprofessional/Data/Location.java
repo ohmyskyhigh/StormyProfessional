e53d0183533a05dfe74f9b9b885ba75eebdcdfc1
ohmyskyhigh <miaorunkun@gmail.com>
2016-07-06 16:40:15 +0800

    V0.0.2
    nailed the url

---
+++ app/src/main/java/com/example/miaor/stormyprofessional/Data/Location.java
@@ -0,0 +1,16 @@
+package com.example.miaor.stormyprofessional.Data;
+
+/**
+ * created by the one and only, Runkun Miao!!!!!!!!!
+ */
+public class Location {
+    private String Zhangjiagang = "31.883, 120.629";
+
+    public String getZhangjiagang() {
+        return Zhangjiagang;
+    }
+
+    public void setZhangjiagang(String zhangjiagang) {
+        Zhangjiagang = zhangjiagang;
+    }
+}
