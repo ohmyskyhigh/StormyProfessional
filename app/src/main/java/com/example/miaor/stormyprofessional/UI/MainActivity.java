dea2fc21908f46a62bd8d2df6ff97178b0a0a2b4
ohmyskyhigh <miaorunkun@gmail.com>
2016-07-06 16:27:17 +0800

    V0.0.1

--- app/src/main/java/com/example/miaor/stormyprofessional/UI/MainActivity.java
+++ app/src/main/java/com/example/miaor/stormyprofessional/UI/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.miaor.stormyprofessional;
+package com.example.miaor.stormyprofessional.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.miaor.stormyprofessional.R;
 
 public class MainActivity extends AppCompatActivity {
 
