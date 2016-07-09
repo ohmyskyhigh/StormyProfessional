ba7cbb4a8660eddcdc28889342638d1246d05b3e
ohmyskyhigh <miaorunkun@gmail.com>
2016-07-09 11:06:05 +0800

    V1.0.2
    Finished Forecast Data Structure

--- app/src/main/java/com/example/miaor/stormyprofessional/Data/Forecast.java
+++ app/src/main/java/com/example/miaor/stormyprofessional/Data/Forecast.java
@@ -4,4 +4,31 @@ package com.example.miaor.stormyprofessional.Data;
  * created by the one and only, Runkun Miao!!!!!!!!!
  */
 public class Forecast {
+    private Current mCurrent;
+    private Day mDay;
+    private Hour mHour;
+
+    public Current getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(Current current) {
+        mCurrent = current;
+    }
+
+    public Day getDay() {
+        return mDay;
+    }
+
+    public void setDay(Day day) {
+        mDay = day;
+    }
+
+    public Hour getHour() {
+        return mHour;
+    }
+
+    public void setHour(Hour hour) {
+        mHour = hour;
+    }
 }
