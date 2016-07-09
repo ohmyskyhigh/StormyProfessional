710c24fe5f43f1fe71334a75d6826f36dc20aea8
ohmyskyhigh <miaorunkun@gmail.com>
2016-07-09 21:09:19 +0800

    V1.5.2
    start a new activity

---
+++ app/src/main/java/com/example/miaor/stormyprofessional/UI/HourlyForecastActivity.java
@@ -0,0 +1,16 @@
+package com.example.miaor.stormyprofessional.UI;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.miaor.stormyprofessional.R;
+
+public class HourlyForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+    }
+}
