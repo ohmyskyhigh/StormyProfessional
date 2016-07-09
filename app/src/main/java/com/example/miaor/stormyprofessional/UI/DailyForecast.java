271445d448ba75cc328958ab0d6719c5ce35c794
ohmyskyhigh <miaorunkun@gmail.com>
2016-07-09 16:28:24 +0800

    V1.0.1
    add dailyForecast Activity

---
+++ app/src/main/java/com/example/miaor/stormyprofessional/UI/DailyForecast.java
@@ -0,0 +1,16 @@
+package com.example.miaor.stormyprofessional.UI;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.miaor.stormyprofessional.R;
+
+public class DailyForecast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+    }
+}
