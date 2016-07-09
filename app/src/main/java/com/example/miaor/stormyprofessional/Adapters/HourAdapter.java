2c6d1ccb04472c9dd011ec7c51ad6621ae0b50ed
ohmyskyhigh <miaorunkun@gmail.com>
2016-07-09 22:16:58 +0800

    V1.6.3

--- app/src/main/java/com/example/miaor/stormyprofessional/Adapters/HourAdapter.java
+++ app/src/main/java/com/example/miaor/stormyprofessional/Adapters/HourAdapter.java
@@ -1,8 +1,54 @@
 package com.example.miaor.stormyprofessional.Adapters;
 
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.miaor.stormyprofessional.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * created by the one and only, Runkun Miao!!!!!!!!!
  */
-public class HourAdapter  {
+public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
+
+
+    @Override
+    public HourViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+
+    @Override
+    public void onBindViewHolder(HourViewHolder holder, int position) {
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class HourViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.SummaryTextHourlyList)
+        TextView mSummary;
+        @BindView(R.id.TimeLabelHourlyList)
+        TextView mTimeLabel;
+        @BindView(R.id.iconImageHourlyList)
+        ImageView mIconImage;
+
+        public HourViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+        }
+    }
 }
